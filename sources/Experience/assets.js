d0058a115b89edc691f3a66859d8305b567b054c
Nicolas NEVEU <nicolas.neveu06@gmail.com>
2023-11-23 17:48:50 +0100

    rename scene 1

--- sources/Experience/assets.js
+++ sources/Experience/assets.js
@@ -7,7 +7,7 @@ export default [
                 { name: 'noiseTex', source: '/assets/ScaleTexture.png', type: 'texture' },
                 { name: 'paperTex', source: '/assets/paperTex.jpg', type: 'texture' },
                 { name: '5mat', source: '/assets/test5Mat.glb' },
-                { name: 'scene_1', source: '/assets/scene_1_v4.glb' },
+                { name: 'scene_1', source: '/assets/scene_1.glb' },
                 { name: 'scene_2', source: '/assets/scene_2.glb' },
                 { name: 'scene_3', source: '/assets/scene_3.glb' },
                 { name: 'scene_4', source: '/assets/scene_4.glb' },
